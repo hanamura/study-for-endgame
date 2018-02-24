f58b7ebbbf41aa2f0a3f1c884cdec37e1a13918e
Taro Hanamura <m@hanamurataro.com>
2018-02-24 16:39:47 +0900

    Input only on change

--- src/main.ino
+++ src/main.ino
@@ -3,6 +3,9 @@
 #define PIN_LED 17
 #define PIN_SWITCH 3
 
+bool currentState = LOW;
+bool lastState = LOW;
+
 void setup() {
   pinMode(PIN_SWITCH, INPUT);
   pinMode(PIN_LED, OUTPUT);
@@ -12,7 +15,13 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(PIN_SWITCH) == HIGH) {
+  currentState = digitalRead(PIN_SWITCH);
+
+  if (currentState == lastState) {
+    return;
+  }
+
+  if (currentState == HIGH) {
     Serial.println("Push");
     digitalWrite(PIN_LED, LOW);
     Keyboard.press(0x61);
@@ -21,4 +30,6 @@ void loop() {
     digitalWrite(PIN_LED, HIGH);
     Keyboard.release(0x61);
   }
+
+  lastState = currentState;
 }
