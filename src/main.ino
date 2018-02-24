f10ad8962a45ec05712f1e45318384cdddc3b0f6
Taro Hanamura <m@hanamurataro.com>
2018-02-24 19:18:51 +0900

    Four keys with minimal matrix

--- src/main.ino
+++ src/main.ino
@@ -1,20 +1,35 @@
 #include "Keyboard.h"
 
-const int numKeys = 3;
+const int numRows = 2;
+const int numCols = 2;
 
-const int pins[numKeys] = {3, 4, 5};
-const byte codes[numKeys] = {0x61, 0x62, 0x63};
+const int rowPins[numRows] = {3, 4};
+const int colPins[numCols] = {5, 6};
+const byte codes[numRows][numCols] = {
+  {0x61, 0x62},
+  {0x63, 0x64}
+};
 
-bool currState[numKeys];
-bool lastState[numKeys];
+bool currState[numRows][numCols];
+bool lastState[numRows][numCols];
 
-int i;
+int i, j;
 
 void setup() {
-  for (i = 0; i < numKeys; i++) {
-    pinMode(pins[i], INPUT);
-    currState[i] = LOW;
-    lastState[i] = LOW;
+  for (i = 0; i < numRows; i++) {
+    pinMode(rowPins[i], OUTPUT);
+  }
+
+  for (i = 0; i < numCols; i++) {
+    pinMode(colPins[i], INPUT);
+  }
+
+  for (i = 0; i < numRows; i++) {
+    for (j = 0; j < numCols; j++) {
+      currState[i][j] = LOW;
+      lastState[i][j] = LOW;
+    }
+    digitalWrite(rowPins[i], LOW);
   }
 
   Serial.begin(9600);
@@ -22,21 +37,31 @@ void setup() {
 }
 
 void loop() {
-  for (i = 0; i < numKeys; i++) {
-    currState[i] = digitalRead(pins[i]);
+  for (i = 0; i < numRows; i++) {
+    digitalWrite(rowPins[i], HIGH);
 
-    if (currState[i] != lastState[i]) {
-      Serial.print("Key: ");
-      Serial.print(i);
-      if (currState[i] == HIGH) {
-        Serial.println(" Down");
-        Keyboard.press(codes[i]);
-      } else {
-        Serial.println(" Up");
-        Keyboard.release(codes[i]);
+    for (j = 0; j < numCols; j++) {
+      currState[i][j] = digitalRead(colPins[j]);
+
+      if (currState[i][j] != lastState[i][j]) {
+        Serial.print("Key: (");
+        Serial.print(i);
+        Serial.print(",");
+        Serial.print(j);
+        Serial.print(")");
+
+        if (currState[i][j] == HIGH) {
+          Serial.println(" Down");
+          Keyboard.press(codes[i][j]);
+        } else {
+          Serial.println(" Up");
+          Keyboard.release(codes[i][j]);
+        }
       }
+
+      lastState[i][j] = currState[i][j];
     }
 
-    lastState[i] = currState[i];
+    digitalWrite(rowPins[i], LOW);
   }
 }
