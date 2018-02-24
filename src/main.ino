4260ad549465718034726d4be0af8ca4366e4317
Taro Hanamura <m@hanamurataro.com>
2018-02-24 17:46:36 +0900

    Two switches

--- src/main.ino
+++ src/main.ino
@@ -2,13 +2,18 @@
 
 #define PIN_LED 17
 #define PIN_A 3
+#define PIN_B 4
 #define KC_A 0x61
+#define KC_B 0x62
 
 bool aCurrState = LOW;
 bool aLastState = LOW;
+bool bCurrState = LOW;
+bool bLastState = LOW;
 
 void setup() {
   pinMode(PIN_A, INPUT);
+  pinMode(PIN_B, INPUT);
   pinMode(PIN_LED, OUTPUT);
 
   Serial.begin(9600);
@@ -18,19 +23,29 @@ void setup() {
 void loop() {
   aCurrState = digitalRead(PIN_A);
 
-  if (aCurrState == aLastState) {
-    return;
+  if (aCurrState != aLastState) {
+    if (aCurrState == HIGH) {
+      Serial.println("A Down");
+      digitalWrite(PIN_LED, LOW);
+      Keyboard.press(KC_A);
+    } else {
+      Serial.println("A Up");
+      digitalWrite(PIN_LED, HIGH);
+      Keyboard.release(KC_A);
+    }
+    aLastState = aCurrState;
   }
 
-  if (aCurrState == HIGH) {
-    Serial.println("Push");
-    digitalWrite(PIN_LED, LOW);
-    Keyboard.press(KC_A);
-  } else {
-    Serial.println("Release");
-    digitalWrite(PIN_LED, HIGH);
-    Keyboard.release(KC_A);
-  }
+  bCurrState = digitalRead(PIN_B);
 
-  aLastState = aCurrState;
+  if (bCurrState != bLastState) {
+    if (bCurrState == HIGH) {
+      Serial.println("B Down");
+      Keyboard.press(KC_B);
+    } else {
+      Serial.println("B Up");
+      Keyboard.release(KC_B);
+    }
+    bLastState = bCurrState;
+  }
 }
