29f0067fd19825d6dfe90eb8e88151fdfa8460d2
Taro Hanamura <m@hanamurataro.com>
2018-02-24 16:32:33 +0900

    Make first keyboard

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,5 @@
+#include "Keyboard.h"
+
 #define PIN_LED 17
 #define PIN_SWITCH 3
 
@@ -6,14 +8,17 @@ void setup() {
   pinMode(PIN_LED, OUTPUT);
 
   Serial.begin(9600);
+  Keyboard.begin();
 }
 
 void loop() {
   if (digitalRead(PIN_SWITCH) == HIGH) {
     Serial.println("Push");
     digitalWrite(PIN_LED, LOW);
+    Keyboard.press(0x61);
   } else {
     Serial.println("Release");
     digitalWrite(PIN_LED, HIGH);
+    Keyboard.release(0x61);
   }
 }
